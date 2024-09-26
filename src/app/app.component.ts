cea9d7650812f62c0dcd97d943fda948629ee8b1
LushoCallapa <luiscallapa2004@gmail.com>
2024-09-25 20:35:27 -0400

    [25-09-24] destructuring, spreed, rest operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 interface IPerson{
   name:string,
   lastName: string,
-  age: number
+  age?: number
 }
 
 @Component({
@@ -19,17 +19,31 @@ export class AppComponent {
   animals: string[] = ['a','b','c','d','e','f','g'];
   
   person: IPerson = {
-    name: 'a',
-    lastName: 'a',
-    age: 12
+    name: 'Luis',
+    lastName: 'Callapa',
+    age: 20
   }
+
+  pets: number[] = [1,2,3,4,5];
+  parents: number[] = [7,8,9,10];
   constructor () {
-    console.log('substract', this.subtract(8,4));
-    console.log('MAP: ',this.animals.map((animal) => (animal + 'new') ));
-    console.log('FOREACH: ',this.animals.forEach((animal) => (animal + 'new') ));
-    console.log('FIND: ',this.animals.find((animal) => (animal === 'b') ));
-    console.log('FIND: ',this.animals.filter((animal) => (animal === 'c') ));
-    console.log('INDEXOF: ',this.animals.indexOf('c'));
+    const {name,age} = this.person;
+    console.log('destructuracion',name+' '+ age);
+    let both = {...this.pets,...this.parents};
+    console.log("Spreed Operator",both)
+
+    console.log("Rest Operator", this.sum2(1,2,3,4,5))
+    // console.log('substract', this.subtract(8,4));
+    // console.log('MAP: ',this.animals.map((animal) => (animal + 'new') ));
+    // console.log('FOREACH: ',this.animals.forEach((animal) => (animal + 'new') ));
+    // console.log('FIND: ',this.animals.find((animal) => (animal === 'b') ));
+    // console.log('FIND: ',this.animals.filter((animal) => (animal === 'c') ));
+    // console.log('INDEXOF: ',this.animals.indexOf('c'));
+  }
+
+  public sum2(...persons: number[]){
+    //return persons[0] + persons[1];
+    return persons.reduce((acc, valor) => (acc+valor) , 0);
   }
 
   public sum(num1: number, num2: number ): number{
@@ -44,7 +58,7 @@ export class AppComponent {
     const persons: number[] = [1,2,3,4,5];
     for(let i =0; i < persons.length;i++){
         if(persons[i]%2 == 0){
-          console.log('person =', persons[i])
+          //console.log('person =', persons[i])
         }
     }
   }
