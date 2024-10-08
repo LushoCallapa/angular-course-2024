aad9117bba909bfb9bc8fefbffa021b54cca661f
LushoCallapa <luiscallapa2004@gmail.com>
2024-10-08 16:27:39 -0400

    [08-10-2024] Previoous Firt Test

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Shared1Component } from './shared1/shared1.component';
+import { Shared2Directive } from './shared2/shared2.directive';
+import { Shared3Pipe } from './shared3/shared3.pipe';
+
+
+
+@NgModule({
+  declarations: [Shared1Component,Shared2Directive,Shared3Pipe],
+  exports: [
+    Shared1Component,
+    Shared2Directive,
+    Shared3Pipe,
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
