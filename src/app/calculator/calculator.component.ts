4c160f3162131979b02320ffaceb775363636a37
LushoCallapa <luiscallapa2004@gmail.com>
2024-10-26 09:15:06 -0400

    [24-10-2024] global service

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'calculator',
@@ -15,6 +16,7 @@ import { ActivatedRoute } from '@angular/router';
           <button class="sum" (click)="onSum()">Sum</button>
           <button class="mul" (click)="onMul()">Mul</button>
           <button class="reset" (click)="onReset()">Reset</button>
+          <button class="reset" (click)="onLogin()">Login</button>
       </div>
     </div>
   `,
@@ -29,7 +31,7 @@ export class CalculatorComponent implements OnInit{
   @Output() mul = new EventEmitter()
   @Output() reset = new EventEmitter()
 
-  constructor(private _activatedRoute: ActivatedRoute) { }
+  constructor(private _activatedRoute: ActivatedRoute,  private _authService: AuthService) { }
   ngOnInit(): void {
       this._activatedRoute.queryParams.subscribe(params => {
         console.log('query params: ', params)
@@ -42,6 +44,10 @@ export class CalculatorComponent implements OnInit{
       type: "SUM",
       result: this.box1Value + this.box2Value,
     });
+  }
+
+  onLogin(){
+    this._authService.login()
   }
 
   public onMul(){
