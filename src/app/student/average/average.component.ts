a5b5be06d79bd422e8d4aa7235ad798a4bd18cc9
LushoCallapa <luiscallapa2004@gmail.com>
2024-10-27 17:46:51 -0400

    [24-10-2024] simple  service

--- src/app/student/average/average.component.ts
+++ src/app/student/average/average.component.ts
@@ -1,15 +1,21 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../auth.service';
+import { ExamService } from '../exam.service';
 
 @Component({
-  selector: 'app-average',
-  templateUrl: './average.component.html',
-  styleUrl: './average.component.scss'
+  selector: "app-average",
+  templateUrl: "./average.component.html",
+  styleUrl: "./average.component.scss",
 })
 export class AverageComponent {
-  
-  constructor(private _authService: AuthService) { }
-  onPrint(){
-    console.log('LOGGED USER: ', this._authService.getUser())
+  constructor(
+    private _authService: AuthService,
+    private _examService: ExamService
+  ) {}
+  onPrint() {
+    console.log("LOGGED USER: ", this._authService.getUser());
+  }
+  onSend(){
+    this._examService.sendNewScore([79,80,60])
   }
 }
