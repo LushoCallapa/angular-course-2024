535861394b2edcb3b404b6f94c9baa9f71cf150e
LushoCallapa <luiscallapa2004@gmail.com>
2024-10-28 14:18:24 -0400

    [25-010-2024] guard CanActivate

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,66 +1,75 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'calculator',
+  selector: "calculator",
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RouterLink],
   template: `
     <div class="calculator">
       <p>Calculator</p>
-      <input type="text" [(ngModel)]="box1Value" >
-      <input type="text" [(ngModel)]="box2Value">
+      <input type="text" [(ngModel)]="box1Value" />
+      <input type="text" [(ngModel)]="box2Value" />
       <div class="buttons-container">
-          <button class="sum" (click)="onSum()">Sum</button>
-          <button class="mul" (click)="onMul()">Mul</button>
-          <button class="reset" (click)="onReset()">Reset</button>
-          <button class="reset" (click)="onLogin()">Login</button>
+        <button class="sum" (click)="onSum()">Sum</button>
+        <button class="mul" (click)="onMul()">Mul</button>
+        <button class="reset" (click)="onReset()">Reset</button>
+        <button class="reset" (click)="onLogin()">Login</button>
+        <button class="reset" [routerLink]="'/student'">
+          Login without token
+        </button>
       </div>
     </div>
   `,
-  styleUrl: './calculator.component.scss'
+  styleUrl: "./calculator.component.scss",
 })
-export class CalculatorComponent implements OnInit{
-
+export class CalculatorComponent implements OnInit {
   box1Value: number = 0;
   box2Value: number = 0;
 
-  @Output() sum = new EventEmitter()
-  @Output() mul = new EventEmitter()
-  @Output() reset = new EventEmitter()
+  @Output() sum = new EventEmitter();
+  @Output() mul = new EventEmitter();
+  @Output() reset = new EventEmitter();
 
-  constructor(private _activatedRoute: ActivatedRoute,  private _authService: AuthService) { }
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _authService: AuthService,
+    private _router: Router
+  ) {}
   ngOnInit(): void {
-      this._activatedRoute.queryParams.subscribe(params => {
-        console.log('query params: ', params)
-      })
-      console.log('query params snapshot: ', this._activatedRoute.snapshot.queryParams)
+    this._activatedRoute.queryParams.subscribe((params) => {
+      console.log("query params: ", params);
+    });
+    console.log(
+      "query params snapshot: ",
+      this._activatedRoute.snapshot.queryParams
+    );
   }
 
-  public onSum(){
+  public onSum() {
     this.sum.emit({
       type: "SUM",
       result: this.box1Value + this.box2Value,
     });
   }
 
-  onLogin(){
-    this._authService.login()
+  onLogin() {
+    this._authService.login();
+    this._router.navigate(['/student'])
   }
 
-  public onMul(){
+  public onMul() {
     this.mul.emit({
       type: "MUL",
       result: this.box1Value * this.box2Value,
     });
   }
 
-  public onReset(){
+  public onReset() {
     this.box1Value = 0;
     this.box2Value = 0;
     this.reset.emit(null);
   }
-
 }
